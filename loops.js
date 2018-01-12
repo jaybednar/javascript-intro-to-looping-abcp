b79e8096d889a5a28825276adc4491de388a8263
Jay Bednar <ironviralmedia@gmail.com>
2018-01-12 19:43:18 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {
+  for (let i = 1; i < 26; i++) {
+    array.push(`I am ${i} strange loop`);
+  }
+  return array;
+}
+
+function whileLoop(number) {
+  while (number >= 0) {
+    console.log(number);
+  }
+  numb
+}
