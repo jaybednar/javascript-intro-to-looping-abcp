8988bc3595b9112b6d93d3d9646b2e2f79a09613
Jay Bednar <ironviralmedia@gmail.com>
2018-01-12 19:57:36 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for (let i = 1; i < 26; i++) {
+    array.push(`I am ${i} strange loop`);
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  Math.random() >= 0.5;
+  do {
+    array.pop();
+  } while (array.length >= 0 && maybeTrue());
+  return array;
+}
